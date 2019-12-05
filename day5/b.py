9a26304abcf94c54abffdbe09df9d7a8c2b484bb
David Pintor <david.pintor@bjss.com>
2019-12-05 06:49:06 +0000

    day 5

---
+++ day5/b.py
@@ -0,0 +1,99 @@
+import sys
+
+def get_opscode(x):
+    return x%100
+
+def get_modes(x):
+    modes = str(x)[:-2]
+    lm=len(modes)
+    if lm < 3:
+        modes = '0'*(3-lm) + modes
+    return modes
+
+def calc_value(prog,mode,pos):
+    if mode == '0':
+        return prog[pos]
+    elif mode == '1':
+        return pos
+    else:
+        print('error')
+        print(mode)
+        sys.exit(1)
+
+def main():
+    filename = 'input.txt'
+
+    with open(filename) as f:
+        program = [int(x) for x in f.readline().split(",")]
+
+    input_instr = 5
+
+    # Run the program
+    position = 0
+    opscode = get_opscode(program[position])
+    modes = get_modes(program[position])
+
+    while opscode != '99':
+        opscode = get_opscode(program[position])
+        modes = get_modes(program[position])
+        if opscode == 1:
+            p1=calc_value(program,modes[2],position+1)
+            p2=calc_value(program,modes[1],position+2)
+            p3=calc_value(program,modes[0],position+3)
+            program[p3] = program[p1] + program[p2]
+            position += 4
+        elif opscode == 2:
+            p1=calc_value(program,modes[2],position+1)
+            p2=calc_value(program,modes[1],position+2)
+            p3=calc_value(program,modes[0],position+3)
+            program[p3] = program[p1] * program[p2]
+            position += 4
+        elif opscode == 3:
+            program[program[position+1]] = input_instr
+            position += 2
+        elif opscode == 4:
+            output = program[program[position+1]]
+            print(output)
+            position += 2
+        elif opscode == 5:
+            p1=calc_value(program,modes[2],position+1)
+            p2=calc_value(program,modes[1],position+2)
+            if program[p1] != 0:
+                position = program[p2]
+            else:
+                position += 3
+        elif opscode == 6:
+            p1=calc_value(program,modes[2],position+1)
+            p2=calc_value(program,modes[1],position+2)
+            if program[p1] == 0:
+                position = program[p2]
+            else:
+                position += 3
+        elif opscode == 7:
+            p1=calc_value(program,modes[2],position+1)
+            p2=calc_value(program,modes[1],position+2)
+            p3=calc_value(program,modes[0],position+3)
+            if program[p1] < program[p2]:
+                program[p3] = 1
+            else:
+                program[p3] = 0
+            position += 4
+        elif opscode == 8:
+            p1=calc_value(program,modes[2],position+1)
+            p2=calc_value(program,modes[1],position+2)
+            p3=calc_value(program,modes[0],position+3)
+            if program[p1] == program[p2]:
+                program[p3] = 1
+            else:
+                program[p3] = 0
+            position += 4
+        elif opscode == 99:
+            break
+        else:
+            print("Wrong code")
+            print(opscode)
+            sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
